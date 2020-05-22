5c7f0aaa9090bc780a0a16b44888add8a1643fa1
yuanzhe yang <yuanzhey@uci.edu>
2020-05-22 16:52:17 -0700

    clab11 update

--- clab11/BnBSolver.java
+++ clab11/BnBSolver.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.Queue;
+
+import java.beans.BeanDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,24 +12,111 @@ import java.util.List;
  * size as the ith Bed.
  */
 public class BnBSolver {
+    public List<Bear> solvedBears = new ArrayList<>();
+    public List<Bed> solvedBeds = new ArrayList<>();
+
+    private static List<Bear> BearConcatenate(List<Bear> l1, List<Bear> l2){
+        l1.addAll(l2);
+        return l1;
+    }
+
+    private static List<Bed> BedConcatenate(List<Bed> l1, List<Bed> l2){
+        l1.addAll(l2);
+        return l1;
+    }
+
+    private static int getRandomInd(List items) {
+        int pivotIndex = (int) (Math.random() * items.size());
+        return pivotIndex;
+    }
+
+    private static void partitionBear(
+            List<Bear> unsortedBear, Bed pivot,
+            List<Bear> less, List<Bear> equal, List<Bear> greater) {
+        for (Bear i:unsortedBear){
+            if(i.compareTo(pivot) < 0){
+                less.add(i);
+            }else if (i.compareTo(pivot) == 0){
+                equal.add(i);
+            }else{
+                greater.add(i);
+            }
+        }
+    }
+
+    private static void partitionBed(
+            List<Bed> unsortedBed, Bear pivot,
+            List<Bed> less, List<Bed> equal, List<Bed> greater) {
+        for (Bed i:unsortedBed){
+            if(i.compareTo(pivot) < 0){
+                less.add(i);
+            }else if (i.compareTo(pivot) == 0){
+                equal.add(i);
+            }else{
+                greater.add(i);
+            }
+        }
+    }
+
+
+
+    private static List<Bed> quickSortBed(List<Bed> unsortedBed,List<Bear> unsortedBear) {
+        if (unsortedBed.size() <= 1){
+            return unsortedBed;
+        }
+        List<Bed> lessBed = new ArrayList<>();
+        List<Bed> equalBed = new ArrayList<>();
+        List<Bed> greaterBed = new ArrayList<>();
+        List<Bear> lessBear = new ArrayList<>();
+        List<Bear> equalBear = new ArrayList<>();
+        List<Bear> greaterBear = new ArrayList<>();
+
+        Bear pivot = unsortedBear.get(getRandomInd(unsortedBear));
+        partitionBed(unsortedBed,pivot,lessBed,equalBed,greaterBed);
+        partitionBear(unsortedBear,equalBed.get(0),lessBear,equalBear,greaterBear);
+        List<Bed> lessOrEqual = BedConcatenate(quickSortBed(lessBed,lessBear),equalBed);
+        List<Bed> finalList = BedConcatenate(lessOrEqual,quickSortBed(greaterBed,greaterBear));
+        return finalList;
+    }
+
+    private static List<Bear> quickSortBear(List<Bed> unsortedBed,List<Bear> unsortedBear) {
+        if (unsortedBear.size() <= 1){
+            return unsortedBear;
+        }
+        List<Bed> lessBed = new ArrayList<>();
+        List<Bed> equalBed = new ArrayList<>();
+        List<Bed> greaterBed = new ArrayList<>();
+        List<Bear> lessBear = new ArrayList<>();
+        List<Bear> equalBear = new ArrayList<>();
+        List<Bear> greaterBear = new ArrayList<>();
+
+        Bed pivot = unsortedBed.get(getRandomInd(unsortedBed));
+        partitionBear(unsortedBear,pivot,lessBear,equalBear,greaterBear);
+        partitionBed(unsortedBed,equalBear.get(0),lessBed,equalBed,greaterBed);
+        List<Bear> lessOrEqual = BearConcatenate(quickSortBear(lessBed,lessBear),equalBear);
+        List<Bear> finalList = BearConcatenate(lessOrEqual,quickSortBear(greaterBed,greaterBear));
+        return finalList;
+    }
 
     public BnBSolver(List<Bear> bears, List<Bed> beds) {
-        // TODO: Fix me.
+        solvedBeds = quickSortBed(beds,bears);
+        solvedBears = quickSortBear(beds,bears);
+
+
+
     }
 
     /**
      * Returns List of Bears such that the ith Bear is the same size as the ith Bed of solvedBeds().
      */
     public List<Bear> solvedBears() {
-        // TODO: Fix me.
-        return null;
+        return solvedBears;
     }
 
     /**
      * Returns List of Beds such that the ith Bear is the same size as the ith Bear of solvedBears().
      */
     public List<Bed> solvedBeds() {
-        // TODO: Fix me.
-        return null;
+        return solvedBeds;
     }
 }
